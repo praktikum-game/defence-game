9a8666d0610b5a93a87c14e2e18acb5f34837975
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-11-02 11:02:12 +0300

    fix configs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,10 @@
 import React, { FC } from 'react';
 import './App.css';
 
-
-
-const App:FC = () => {
-    return (
-        <div>
-            <h1>Мое супер приложение</h1>
-        </div>
-
-    )
-}
+const App: FC = () => (
+  <div>
+    <h1>Мое супер приложение</h1>
+  </div>
+);
 
 export default App;
