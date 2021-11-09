72b4cb64a8f8702d89d6c3bc02e11c55517de2aa
Verisana <account@0112.pro>
2021-11-09 12:04:03 +0500

    feat: add test Link component to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react';
+import { Link } from '../Link';
 import './App.css';
 
 const App: FC = () => (
   <div className="title">
     <h1>Мое супер приложение</h1>
+    <Link href="test">Link name</Link>
   </div>
 );
 
