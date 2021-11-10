61ebc260f7a1268ac167e8d07a1e23eea52af069
Natalya Garipova <webvakansii@yandex.ru>
2021-11-10 17:07:34 +0300

    added postcss-simple-vars

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -6,7 +6,7 @@ export const ProfilePage = (): JSX.Element => (
   <>
     <div className="profile-page">
       <div className="logo">
-        <img src="/images/logo.png" alt="DoctorsVsViruses" />
+        <img src="/images/logo.png" alt="Doctors Vs Viruses" />
       </div>
       <h1 className="profile-page_name">Страница профиля</h1>
       <p>Контент страницы</p>
