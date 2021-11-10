57b0574a28dd38cb01735bb9a14d22201563b128
Natalya Garipova <webvakansii@yandex.ru>
2021-11-10 08:22:08 +0300

    added profile-page

---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './ProfilePage.css';
+
+export const ProfilePage = (): JSX.Element => (
+  <div className="profile-page">
+    <h1>Страница профиля</h1>
+    <p>Контент страницы</p>
+  </div>
+);
