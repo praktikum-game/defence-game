42046c5f1f142117e5b8c7fcee992a153d9269f2
Natalya Garipova <webvakansii@yandex.ru>
2021-11-10 15:48:31 +0300

    added postcss plugins

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import './ProfilePage.css';
 
 export const ProfilePage = (): JSX.Element => (
-  <div className="profile-page">
-    <h1>Страница профиля</h1>
-    <p>Контент страницы</p>
-  </div>
+  <>
+    <div className="profile-page">
+      <div className="logo">
+        <img src="/images/logo.png" alt="DoctorsVsViruses" />
+      </div>
+      <h1 className="profile-page_name">Страница профиля</h1>
+      <p>Контент страницы</p>
+    </div>
+  </>
 );
