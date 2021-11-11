5d7ab84a73894207b1ad83fd54d95de89317dd08
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-11-11 22:25:06 +0300

    implement leaderboard table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,28 @@ import React, { FC, useCallback, useEffect, useState } from 'react';
 import { email, required, validate, ValidationResult } from '../../utilites/validator';
 import { InputField } from '../InputField';
 import { Button } from '../Button';
+import { LeaderboardTable, LeaderboardTableProps } from '../LeaderboardTable';
 import './App.css';
 
 const inputValidators = [
   { checkFunction: required(), message: 'Это обязательное поле' },
   { checkFunction: email(), message: 'Это не email' },
 ];
+
+const tableData: LeaderboardTableProps = {
+  columns: [
+    { dataId: 'username', title: 'Имя пользователя' },
+    { dataId: 'login', title: 'Логин' },
+    { dataId: 'score', title: 'Колиество очков' },
+  ],
+
+  data: [
+    { id: 'a123', login: 'vasya', username: 'Василий Алибабаевич', score: 168 },
+    { id: 'a124', login: 'kosoy', username: 'Косой', score: 202 },
+    { id: 'a125', login: 'hmyr', username: 'Хмырь', score: 386 },
+    { id: 'a126', login: 'docent', username: 'Доцент', score: 409 },
+  ],
+};
 
 const App: FC = () => {
   const [inputValue, setInputValue] = useState<string>('');
@@ -44,6 +60,8 @@ const App: FC = () => {
       />
 
       <Button onClick={handleButtonClick} text="MyButton" view="primary"></Button>
+
+      <LeaderboardTable {...tableData} />
     </div>
   );
 };
--- src/components/LeaderboardTable/LeaderboardTable.tsx
+++ src/components/LeaderboardTable/LeaderboardTable.tsx
@@ -1,30 +1,25 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { block } from 'bem-cn';
-import './index.css';
+import { LeaderboardItem, LeaderboardTableProps } from '.';
+import './leaderboard-table.css';
 
 const b = block('lead-table');
 
-export const LbTable = ({ data, columns }: LeaderboardTableProps) => {
-  useEffect(() => {}, []);
-
-  return (
-    <div className={b()}>
-      <div className={b('row', { header: true, rounded: true })}>
-        {columns.map((colHeader, idx) => (
-          <div className={b('cell', { border: idx < columns.length - 1 })}>{colHeader.title}</div>
-        ))}
-      </div>
-      {data
-        .sort((a: LeaderboardItem, c: LeaderboardItem) => c.score - a.score)
-        .map((row) => (
-          <div key={row.id} className={b('row', { rounded: true })}>
-            {columns.map((col, idx) => (
-              <div className={b('cell', { border: idx < columns.length - 1 })}>
-                {row[col.dataId]}
-              </div>
-            ))}
-          </div>
-        ))}
+export const LeaderboardTable = ({ data, columns }: LeaderboardTableProps) => (
+  <div className={b()}>
+    <div className={b('row', { header: true, rounded: true })}>
+      {columns.map((colHeader, idx) => (
+        <div className={b('cell', { border: idx < columns.length - 1 })}>{colHeader.title}</div>
+      ))}
     </div>
-  );
-};
+    {data
+      .sort((item: LeaderboardItem, anotherItem: LeaderboardItem) => anotherItem.score - item.score)
+      .map((row) => (
+        <div key={row.id} className={b('row', { rounded: true })}>
+          {columns.map((col, idx) => (
+            <div className={b('cell', { border: idx < columns.length - 1 })}>{row[col.dataId]}</div>
+          ))}
+        </div>
+      ))}
+  </div>
+);
