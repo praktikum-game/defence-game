7e3b34f4606416bdc49213801bb3790196c46cf4
Shvetsov Yura <oygemor@yandex.ru>
2021-11-11 20:48:26 +0300

    lb

---
+++ src/components/LeaderboardTable/types.ts
@@ -0,0 +1,16 @@
+export type LeaderboardItem = {
+  id: string;
+  username: string;
+  login: string;
+  score: number;
+};
+
+export type LeaderboardTableColumn = {
+  title: string;
+  dataId: keyof LeaderboardItem;
+};
+
+export type LeaderboardTableProps = {
+  columns: Array<LeaderboardTableColumn>;
+  data: Array<LeaderboardItem>;
+};
