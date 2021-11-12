fda6420787984e9a65913af5d92403f31f8d52b1
Natalya Garipova <webvakansii@yandex.ru>
2021-11-12 15:11:36 +0300

    added file-loader to webpack

--- src/components/AppRoutes/routes.tsx
+++ src/components/AppRoutes/routes.tsx
@@ -11,53 +11,57 @@ import { ForumThreadPage } from '../../pages/ForumPage/components/ForumThreadPag
 import { ForumIndexPage } from '../../pages/ForumPage/components/ForumIndexPage';
 import { ErrorPage404 } from '../../pages/ErrorPage404';
 import { ErrorPage500 } from '../../pages/ErrorPage500';
+import { PageLayout } from '../PageLayout';
 
 export const routes: RouteObject[] = [
   {
     path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/profile',
-    element: <ProfilePage />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/register',
-    element: <RegisterPage />,
-  },
-  {
-    path: '/game',
-    element: <GamePage />,
-  },
-  {
-    path: '/ratings',
-    element: <RatingsPage />,
-  },
-  {
-    path: '/forum',
-    element: <ForumPage />,
+    element: <PageLayout />,
     children: [
-      { index: true, element: <ForumIndexPage /> },
+      { index: true, element: <HomePage /> },
       {
-        path: ':forumId',
-        element: <ForumThreadPage />,
+        path: '/profile',
+        element: <ProfilePage />,
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/register',
+        element: <RegisterPage />,
+      },
+      {
+        path: '/game',
+        element: <GamePage />,
+      },
+      {
+        path: '/ratings',
+        element: <RatingsPage />,
+      },
+      {
+        path: '/forum',
+        element: <ForumPage />,
+        children: [
+          { index: true, element: <ForumIndexPage /> },
+          {
+            path: ':forumId',
+            element: <ForumThreadPage />,
+          },
+        ],
+      },
+      {
+        path: '500',
+        element: <ErrorPage500 />,
+      },
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '*',
+        element: <ErrorPage404 />,
       },
     ],
-  },
-  {
-    path: '500',
-    element: <ErrorPage500 />,
-  },
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '*',
-    element: <ErrorPage404 />,
   },
 ];
