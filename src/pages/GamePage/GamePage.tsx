90ce1fe2bc7cf2dc97e1feb3f76fcc223840a3ad
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-11-12 13:53:26 +0300

    move game page

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { FC, useEffect, useRef } from 'react';
+import Game from '../../game/Game';
 
-export const GamePage = (): JSX.Element => (
-  <>
-    <h1>Игра</h1>
-    <p>Тут будет игра</p>
-  </>
-);
+export const GamePage: FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d');
+    if (ctx) {
+      const game = new Game(ctx);
+      game.run();
+    }
+  }, []);
+  return <canvas ref={canvasRef} height={500} width={1200}></canvas>;
+};
