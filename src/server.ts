699e9d9eac291339e1df08adb41b407dd6cd917c
Verisana <account@0112.pro>
2021-11-12 12:34:13 +0500

    style: prettier style changes for server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import express from "express"
-import path from "path"
+import express from 'express';
+import path from 'path';
 
-const PORT = process.env.PORT !== undefined ? process.env.PORT : 3000
+const PORT = process.env.PORT !== undefined ? process.env.PORT : 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "dist")))
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
-app.get("/*", (_0, res) => {
-  res.sendFile(path.join(__dirname, "..", "dist", "index.html"))
-})
+app.get('/*', (_0, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening port number ${PORT}`)
-})
+  console.log(`Listening port number ${PORT}`);
+});
