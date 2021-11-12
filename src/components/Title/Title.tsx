4737e1924fe6085a3c771c81b5618576cc4cfa4f
Verisana <account@0112.pro>
2021-11-12 15:47:52 +0500

    fix: className space usage

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -5,9 +5,9 @@ import { TitleProps } from './types';
 export const Title = ({
   headingLevel = 5,
   align = 'center',
-  className_ = '',
   ...otherProps
 }: TitleProps): JSX.Element => {
   const Component = `h${headingLevel}`;
-  return <Component className={`title title_${align} ${className_}`} {...otherProps}></Component>;
+  const className = otherProps.className_ ? ` ${otherProps.className_}` : '';
+  return <Component className={`title title_${align}${className}`} {...otherProps}></Component>;
 };
