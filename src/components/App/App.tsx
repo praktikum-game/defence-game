4f0b2f7f402baab41507e1765eb5f92cc99339b9
Verisana <account@0112.pro>
2021-11-13 12:52:49 +0500

    feat: create common.css and import in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppRoutes } from '../AppRoutes';
 import './App.css';
+import '../../utilities/common.css';
 
 export const App = () => (
   <div className="App">
