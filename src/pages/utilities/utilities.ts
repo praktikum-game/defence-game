ef8d3457d050ec75feb8e0ac59aae8035271b8e6
Verisana <account@0112.pro>
2021-11-13 14:46:39 +0500

    feat: use lastValue in inputValueUpdaterFactory

--- src/pages/utilities/utilities.ts
+++ src/pages/utilities/utilities.ts
@@ -1,13 +1,13 @@
-import React from "react"
+import React from 'react';
 import { ValidationResult } from '../../utilities/validators';
 
 export function inputValueUpdaterFactory(
-  validator: (value: string) => ValidationResult,
+  validator: (value: string, lastValue?: string) => ValidationResult,
   validationSetter: React.Dispatch<React.SetStateAction<ValidationResult>>,
   valueSetter: React.Dispatch<React.SetStateAction<string>>,
 ) {
-  return (value: string) => {
-    const validationResult = validator(value);
+  return function updateValue(value: string, lastValue?: string) {
+    const validationResult = validator(value, lastValue);
     validationSetter(validationResult);
     valueSetter(value);
   };
