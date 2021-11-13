7357f439aad697d12b925aed8b9706dac37856da
Natalya Garipova <webvakansii@yandex.ru>
2021-11-13 13:48:21 +0300

    rebase with main

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,10 @@ module.exports = {
   ],
   module: {
     rules: [
+      // {
+      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      //   type: 'asset/resource',
+      // },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
