83f2e744a741bef05ec93a4ffec91b452a85e81d
Verisana <account@0112.pro>
2021-11-13 12:01:49 +0500

    feat: update LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -6,24 +6,29 @@ import { Form } from '../../components/Form';
 import { InputField } from '../../components/InputField';
 import { Button } from '../../components/Button';
 import { Title } from '../../components/Title';
-import { loginValidator } from '../../utilities/validators';
+import { loginValidator, passwordValidator, ValidationResult } from '../../utilities/validators';
 
 export const LoginPage = (): JSX.Element => {
   const [loginValue, setLoginValue] = useState('');
-  const [loginErrorText, setLoginErrorText] = useState<string | null>(null);
+  const [loginValidationResult, setLoginValidationResult] = useState<ValidationResult>({
+    message: '',
+    valid: false,
+  });
   const [passwordValue, setPasswordValue] = useState('');
-  const [passwordErrorText, setPasswordErrorText] = useState<string | null>(null);
+  const [passwordValidationResult, setPasswordValidationResult] = useState<ValidationResult>({
+    message: '',
+    valid: false,
+  });
 
   const updateLogin = (value: string) => {
     const validationResult = loginValidator(value);
-    console.log(validationResult);
-    setLoginErrorText(validationResult.message);
+    setLoginValidationResult(validationResult);
     setLoginValue(value);
   };
 
   const updatePassword = (value: string) => {
-    const validationResult = loginValidator(value);
-    setPasswordErrorText(validationResult.message);
+    const validationResult = passwordValidator(value);
+    setPasswordValidationResult(validationResult);
     setPasswordValue(value);
   };
 
@@ -33,22 +38,28 @@ export const LoginPage = (): JSX.Element => {
         <Header>
           <Title>Входи, защитник</Title>
         </Header>
-        <Form>
+        <Form validationResults={[loginValidationResult, passwordValidationResult]}>
           <InputField
             value={loginValue}
+            name="login"
             label="Логин"
-            errorText={loginErrorText}
-            isValid={loginErrorText === null}
+            errorText={loginValidationResult.message}
+            isValid={loginValidationResult.valid}
             valueChangeCallback={updateLogin}
           />
           <InputField
             value={passwordValue}
+            name="password"
             label="Пароль"
-            errorText={passwordErrorText}
-            isValid={passwordErrorText === null}
+            errorText={passwordValidationResult.message}
+            isValid={passwordValidationResult.valid}
             valueChangeCallback={updatePassword}
           />
-          <Button text="Авторизоваться" type="submit" />
+          <Button
+            text="Авторизоваться"
+            type="submit"
+            disabled={!(loginValidationResult.valid && passwordValidationResult.valid)}
+          />
         </Form>
         <Link to="/register">
           <Title>Нет аккаунта?</Title>
