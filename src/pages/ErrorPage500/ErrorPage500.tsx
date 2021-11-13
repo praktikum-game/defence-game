f4141afd28651373dca817b685c8ab2e326d0c28
Verisana <account@0112.pro>
2021-11-13 14:58:51 +0500

    feat: update 505 page

--- src/pages/ErrorPage500/ErrorPage500.tsx
+++ src/pages/ErrorPage500/ErrorPage500.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Title } from '../../components/Title';
 
 export const ErrorPage500 = (): JSX.Element => (
   <>
-    <h1>500</h1>
-    <p>Ошибка на сервере</p>
+    <Title headingLevel={1} align="center">
+      500
+    </Title>
+    <Title headingLevel={2} align="center">
+      Сервер не отвечает
+      <br />
+      :(
+      <br />
+    </Title>
+    <Title headingLevel={3} align="center">
+      Но мы уже работаем над этой проблемой
+    </Title>
   </>
 );
