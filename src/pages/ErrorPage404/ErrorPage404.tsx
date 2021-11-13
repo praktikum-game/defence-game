b725377c2246374826a8ed4ec7988d7647bff4e8
Verisana <account@0112.pro>
2021-11-13 14:58:39 +0500

    feat: update 404 page

--- src/pages/ErrorPage404/ErrorPage404.tsx
+++ src/pages/ErrorPage404/ErrorPage404.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { Title } from '../../components/Title';
 
 export const ErrorPage404 = (): JSX.Element => (
   <>
-    <h1>404</h1>
-    <p>Не туда попали</p>
+    <Title headingLevel={1} align="center">
+      404
+    </Title>
+    <Title headingLevel={2} align="center">
+      Не туда попали
+      <br />
+      :(
+    </Title>
+    <Link to="/game">
+      <Button text="Назад к игре" type="button" className="center-horizontal" />
+    </Link>
   </>
 );
