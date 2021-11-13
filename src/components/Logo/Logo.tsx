7b2617520f8e157b21b304a3afa929ec21904ee4
Natalya Garipova <webvakansii@yandex.ru>
2021-11-13 13:48:21 +0300

    rebase with main

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { LogoProps } from './types';
+import logo from './logo.svg';
 import './logo.css';
 
 export const Logo = (props: LogoProps) => (
   <Link to="/home">
-    <div className="logo" {...props}></div>
+    <div className="logo" {...props}>
+      <img src={logo} alt="Logo" />
+    </div>
   </Link>
 );
