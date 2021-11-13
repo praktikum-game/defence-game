099bdb75b6d2f3ff36d4ead3139f15b0cd9f1e36
Verisana <account@0112.pro>
2021-11-13 14:02:37 +0500

    feat: update classNames on Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -5,9 +5,10 @@ import { TitleProps } from './types';
 export const Title = ({
   headingLevel = 5,
   align = 'center',
+  className_ = '',
   ...otherProps
 }: TitleProps): JSX.Element => {
   const Component = `h${headingLevel}`;
-  const className = otherProps.className_ ? ` ${otherProps.className_}` : '';
+  const className = className_ === '' ? '' : ` ${className_}`;
   return <Component className={`title title_${align}${className}`} {...otherProps}></Component>;
 };
