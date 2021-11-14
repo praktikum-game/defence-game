0a219c90b929daa359794ae161b4c59f38e3eae2
Verisana <account@0112.pro>
2021-11-14 13:31:02 +0500

    feat: create and use ErrorBoundary component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 import { App } from './components/App';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </BrowserRouter>,
   document.querySelector('#root'),
 );
