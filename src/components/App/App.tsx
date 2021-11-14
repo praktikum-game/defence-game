f14daed306b3e5d568047800fa2be925d3623b50
Verisana <account@0112.pro>
2021-11-14 17:44:43 +0500

    feat: fetch user info in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppRoutes } from '../AppRoutes';
+import { AuthController } from '../../controllers';
 import './App.css';
 import '../../utilities/common.css';
 
-export const App = () => (
-  <div className="App">
-    <AppRoutes />
-  </div>
-);
+export const App = () => {
+  useEffect(() => {
+    AuthController.userRead().then(() => {
+
+    });
+  });
+
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+};
