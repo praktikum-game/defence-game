4c5e9a409ed0f2c25ee2e06e7271187c2a9b4921
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-11-15 22:49:22 +0300

    start

--- src/pages/ForumPage/components/ForumIndexPage/ForumIndexPage.tsx
+++ src/pages/ForumPage/components/ForumIndexPage/ForumIndexPage.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Avatar } from '../../../../components/Avatar';
+
+import { Title } from '../../../../components/Title';
 
 export const ForumIndexPage = () => (
   <>
     <h2>Главная страница форума</h2>
-    <p>текст главной страницы форума</p>
+    <div>
+      <div>
+        <div>
+          <Avatar></Avatar>
+          <div>
+            <Title align="left" headingLevel={3}>
+              Мой сладкий сахар
+            </Title>
+            <span>sdfafadsf</span>
+            <span>213</span>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
   </>
 );
