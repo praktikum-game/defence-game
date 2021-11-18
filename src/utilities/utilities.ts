f21a3031b5c39a49acbe8caf3221fe7603ece1a3
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-11-19 00:08:02 +0300

    remove utils functions

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -16,38 +16,3 @@ export function bindArgsFromN(fn: Function, n: number, ...bound_args: unknown[])
   return (...args: unknown[]) => fn(...args.slice(0, n - 1), ...bound_args);
 }
 
-export function getDatetimeObject(date: Date) {
-  const dateParts = {
-    year: date.getFullYear(),
-    month: date.getMonth(),
-    dayOfMonth: date.getDate(),
-    hours: date.getHours(),
-    minutes: date.getMinutes(),
-    seconds: date.getSeconds(),
-  };
-
-  const appendLeadZero = (item: number) => {
-    if (item < 10) {
-      return `0${Number}`;
-    }
-    return `${item}`;
-  };
-
-  const dateTimeObject = Object.fromEntries(
-    Object.entries({ ...dateParts }).map(([key, value]) => [key, appendLeadZero(value)]),
-  ) as DatetimeObject;
-
-  return dateTimeObject;
-}
-
-export function formatDateTime(
-  dtObj: DatetimeObject,
-  dateFields: Array<keyof Partial<DatetimeObject>> = ['dayOfMonth', 'month', 'year'],
-  timeFields: Array<keyof Partial<DatetimeObject>> = ['hours', 'minutes'],
-  dateDelemiter: string = '.',
-  timeDelemiter: string = ':',
-): string {
-  const dateString = dateFields.map((d) => dtObj[d]).join(dateDelemiter);
-  const timeString = timeFields.map((t) => dtObj[t]).join(timeDelemiter);
-  return `${dateString} ${timeString}`;
-}
