7dbcd4c3f296c169294882f856e1bb608c96cabe
Verisana <account@0112.pro>
2021-11-22 13:46:11 +0500

    feat: create serviceWorker file

---
+++ src/serviceWorker.ts
@@ -0,0 +1,29 @@
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+
+// @ts-expect-error
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  ({ request }) => request.destination === 'script' || request.destination === 'style',
+  new StaleWhileRevalidate(),
+);
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
--- src/serviceWorkers.ts
+++
@@ -1,9 +0,0 @@
-this.addEventListener('install', (event: Event) => {
-  console.log(event);
-  console.log('install');
-});
-
-this.addEventListener('activate', (event: Event) => {
-  console.log(event);
-  console.log('activate');
-});
