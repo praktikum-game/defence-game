dbf60df0a45867adfec1a842fd068c681e2a66c8
Verisana <account@0112.pro>
2021-11-22 14:48:31 +0500

    test: create mock test for Button

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+// import { Button } from './Button';
+
+describe('test Button component', () => {
+  it('constructor and props', () => {
+    expect.assertions(1);
+    expect(2).toBe(2);
+  });
+});
