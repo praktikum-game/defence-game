bda5fc558e638e6f8eb5c2967eabd83f12a74590
Verisana <account@0112.pro>
2021-12-02 14:15:33 +0500

    fix: unexisting css import in forumPage

--- src/pages/ForumPage/ForumPage.tsx
+++ src/pages/ForumPage/ForumPage.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-import './forum-page.css';
-
 export const ForumPage = () => (
   <div className="forum-layout-container">
     <Outlet />
