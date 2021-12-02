77039ed53590d8baf42e819dfd080fe8cca32b0b
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-12-02 16:26:34 +0300

    rebase

--- src/pages/ForumPage/ForumPage.tsx
+++ src/pages/ForumPage/ForumPage.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
 
 export const ForumPage = () => {
-  useAuth(false);
   return (
     <div>
       <Outlet />
