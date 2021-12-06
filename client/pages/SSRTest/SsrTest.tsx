220396eef03a2cc84a5a702a7503887b4ac207e1
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-12-06 15:16:23 +0300

    add style loader

--- client/pages/SSRTest/SsrTest.tsx
+++ client/pages/SSRTest/SsrTest.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-//import './style.css';  будет ошибка, если подключить стили
+import './style.css'; //будет ошибка, если подключить стили
 export const SsrTest = () => (
-  <div className="ssr-test-page">Привет! Я - SSR! Но я еще маленький!</div>
+  <div>
+    <p className="ssr-test-page">Привет! Я - SSR! Но я еще маленький!</p>
+  </div>
 );
--- webpack/loaders/css.ts
+++ webpack/loaders/css.ts
@@ -1,8 +1,34 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { ENVS } from '../assets/env';
+
+const postcssLoader = {
+  loader: 'postcss-loader',
+};
+
+const cssLoader = {
+  loader: 'css-loader',
+  options: {
+    sourceMap: ENVS.__DEV__,
+    importLoaders: 1,
+    modules: {
+      localIdentName: ENVS.__DEV__ ? '[name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+    },
+  },
+};
+
+const cssLoaders = [MiniCssExtractPlugin.loader, cssLoader, postcssLoader];
+
 export const css = {
   client: [
     {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: cssLoaders,
+    },
+    {
+      test: /\.css$/,
+      include: /node_modules/,
+      use: [MiniCssExtractPlugin.loader, { loader: 'css-loader' }, postcssLoader],
     },
   ],
   ssr: [{ test: /\.css$/i, loader: 'null-loader' }],
