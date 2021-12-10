679bb3a305190495f87292c916c9627760c0451a
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-12-10 09:13:35 +0300

    add https dev server/certs

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const { app } = require('./dist/ssr.js');
+const { startServer } = require('./dist/ssr.js');
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log('Yo! Bro! Server started on port:', PORT);
-});
+startServer();
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,37 @@
 import { join, resolve } from 'path';
+import { readFileSync } from 'fs';
 import express, { Express } from 'express';
 import compression from 'compression';
 import { ssrHtmlRenderMiddleware } from './ssr-html-render-middleware';
+import https from 'https';
+
+const { PORT = 3001, NODE_ENV } = process.env;
+const isDev = NODE_ENV === 'development';
 
 const app: Express = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(compression());
-}
+const startServer = () => {
+  if (isDev) {
+    app.use(compression());
+
+    const options = {
+      cert: readFileSync(join(__dirname, '..', 'certs', 'cert.crt'), 'utf-8'),
+      key: readFileSync(join(__dirname, '..', 'certs', 'cert.key'), 'utf-8'),
+    };
+
+    https.createServer(options, app).listen(PORT, '0.0.0.0' as any, () => {
+      console.log('\x1b[32m', `START HTTPS DEV SERVER ON PORT:${PORT}`);
+      console.log('\x1b[0m');
+    });
+  }
+
+  if (!isDev) {
+    app.listen(PORT, () => {
+      console.log('\x1b[32m', `START PRODUCTION SERVER ON PORT: ${PORT}`);
+      console.log('\x1b[0m');
+    });
+  }
+};
 
 app
   .use(express.static(resolve(__dirname, '../dist')))
@@ -19,4 +43,4 @@ app.get('/serviceWorker.js', (_0, res) => {
 
 app.get('/*', ssrHtmlRenderMiddleware);
 
-export { app };
+export { startServer };
