6514b199f9ff4e0ef16e9741e1627beb81157dcf
Verisana <account@0112.pro>
2021-12-10 13:43:13 +0500

    feat: add __PRELOADED_STATE__ to global windows type

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,7 +1,17 @@
+export {};
+
 declare module '*png' {
   export default string;
 }
 
 declare module '*svg' {
   export default string;
+}
+
+declare global {
+  interface Window {
+    // В d.ts нам неважно, что это за тип,
+    // так как он сразу попадает в redux store на клиенте
+    __PRELOADED_STATE__?: object;
+  }
 }
