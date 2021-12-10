f8c8e3532c49d990b0e760cbb5eec4f1ad722c8d
Verisana <account@0112.pro>
2021-12-10 18:06:31 +0500

    fix: Navigate warning on server side

--- src/components/RequireAuth/RequireAuth.tsx
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthUser } from '../../hooks/useAuthUser';
 import { RequireAuthProps } from './types';
+import { isServer } from '../../utilities/utilities';
 
-// Опасно вот хардкодить путь. Если он поменяется, то забудем. Лучше бы вынести
+// Опасно вот так хардкодить путь. Если он поменяется, то забудем. Лучше бы вынести
 // в отдельное место все пути
 export const RequireAuth = ({ children, to = '/login', inverse = false }: RequireAuthProps) => {
   const { userData } = useAuthUser();
   const location = useLocation();
 
   if (!inverse && !userData) {
+    if (isServer) {
+      return <></>;
+    }
     return <Navigate replace to={to} state={{ from: location }} />;
   }
 
   if (inverse && userData) {
+    if (isServer) {
+      return <></>;
+    }
     return <Navigate replace to={to} state={{ from: location }} />;
   }
 
