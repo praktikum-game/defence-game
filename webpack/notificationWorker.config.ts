564347d3de51c2b343d029beeec646f976435822
Verisana <account@0112.pro>
2021-12-11 19:07:04 +0500

    feat: workin notificationWorkerConfig

--- webpack/notificationWorker.config.ts
+++ webpack/notificationWorker.config.ts
@@ -1,37 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import { Configuration, DllReferencePlugin } from 'webpack';
+import { Configuration } from 'webpack';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 import { join } from 'path';
 import { DIST_DIR, IS_DEV, SRC_DIR } from './env';
 import { ts } from './loaders';
 
 export const notificationWorkerConfig: Configuration = {
+  entry: join(SRC_DIR, 'notificationWorker.ts'),
   mode: IS_DEV ? 'development' : 'production',
-  entry: {
-    notificationWorker: join(SRC_DIR, 'notificationWorker.ts'),
-  },
   devtool: 'source-map',
   output: {
     path: DIST_DIR,
-    filename: '[name].js',
+    filename: 'notificationWorker.js',
     publicPath: '/',
   },
   module: {
     rules: [ts.client],
   },
   resolve: {
-    modules: ['src/notificationWorker.ts'],
-    extensions: ['.ts'],
+    modules: ['src', 'node_modules'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     plugins: [new TsconfigPathsPlugin()],
   },
-  plugins: [
-    new DllReferencePlugin({
-      context: join(DIST_DIR, '..'),
-      manifest: join(DIST_DIR, 'vendors', 'vendors-manifest.json'),
-    }),
-  ].filter(Boolean) as [],
-
   performance: {
     hints: IS_DEV ? false : 'error',
   },
