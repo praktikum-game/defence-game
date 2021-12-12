3521f414f222e4dc9f00762c43d4f485472f4f19
Verisana <account@0112.pro>
2021-12-12 23:57:01 +0500

    feat: load avatar in srcFull in Avatar component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import block from 'bem-cn';
+import { useSelector } from 'react-redux';
 import { AvatarProps } from './types';
+import { AppState } from '../../store';
 import './avatar.css';
 import defaultAvatar from './static/default-avatar.svg';
 
 const b = block('avatar');
 
-export const Avatar = ({ src = defaultAvatar, className, ...props }: AvatarProps) => (
-  <img className={b.mix(className)} src={src} {...props}></img>
-);
+export const Avatar = ({ src, className, ...props }: AvatarProps) => {
+  const userData = useSelector((state: AppState) => state.user.data);
+  let srcFull = defaultAvatar;
+  if (userData !== null) {
+    srcFull =
+      src === undefined
+        ? userData.avatar !== ''
+          ? userData.avatar
+          : defaultAvatar
+        : defaultAvatar;
+  }
+
+  return <img className={b.mix(className)} src={srcFull} {...props}></img>;
+};
