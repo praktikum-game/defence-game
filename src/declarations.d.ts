25fabf143fd02610eb5d5577fd954bbdc9507d52
Verisana <account@0112.pro>
2021-12-13 16:44:17 +0500

    refactor: add type to LOADED

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,5 @@
+import { AppState } from './store';
+
 export {};
 
 declare module '*png' {
@@ -10,8 +12,6 @@ declare module '*svg' {
 
 declare global {
   interface Window {
-    // В d.ts нам неважно, что это за тип,
-    // так как он сразу попадает в redux store на клиенте
-    __PRELOADED_STATE__?: object;
+    __PRELOADED_STATE__?: AppState;
   }
 }
