50878dddd00b8ac27ff990c97930682101d83e70
Verisana <account@0112.pro>
2021-12-20 21:17:27 +0500

    feat: create BaseCrud model

---
+++ src/server/services/BaseCrud.ts
@@ -0,0 +1,33 @@
+import { ModelDefined } from 'sequelize/types';
+
+export abstract class BaseCrud<T, D> {
+  private _model: ModelDefined<T, D>;
+
+  constructor(model: ModelDefined<T, D>) {
+    this._model = model;
+  }
+
+  readAll() {
+    return this._model.findAll();
+  }
+
+  readById(id: number) {
+    return this._model.findByPk(id);
+  }
+
+  create(data: D) {
+    return this._model.create(data);
+  }
+
+  update(id: number, field: string, value: string) {
+    // Не могу справиться с типами. Нужная помощь
+    // @ts-expect-error
+    return this._model.update({ [field]: value }, { where: { id } });
+  }
+
+  delete(id: number) {
+    // Не могу справиться с типами. Нужная помощь
+    // @ts-expect-error
+    return this._model.destroy({ where: { id } });
+  }
+}
