90f0e78f7f86f15ccad44e948a253677b8e01a0a
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-12-20 16:42:13 +0300

    fix game assets images

--- webpack/plugin-options.ts
+++ webpack/plugin-options.ts
@@ -1,22 +1,18 @@
+//eslint-disable-next-line
+import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin';
+
 export const pluginOptions = {
   imageMinimizerOptions: {
-    minimizerOptions: {
-      plugins: [
-        ['gifsicle', { interlaced: true }],
-        ['jpegtran', { progressive: true }],
-        ['optipng', { optimizationLevel: 5 }],
-        [
-          'svgo',
-          {
-            plugins: [
-              {
-                name: 'removeViewBox',
-                active: false,
-              },
-            ],
-          },
+    minimizer: {
+      implementation: ImageMinimizerPlugin.imageminMinify,
+      options: {
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 6 }],
+          ['svgo'],
         ],
-      ],
+      },
     },
   },
 };
