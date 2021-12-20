08a74903cab3c5f306783b2d2bb0e70ece38587d
Verisana <account@0112.pro>
2021-12-20 12:44:35 +0500

    build: write config for database

---
+++ config/database.ts
@@ -0,0 +1,24 @@
+import { Options } from 'sequelize';
+import pg from 'pg';
+import { config } from 'dotenv';
+
+config();
+
+const { POSTGRES_ORM_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } =
+  process.env;
+
+const sequelizeOptions: Options = {
+  host: POSTGRES_ORM_HOST,
+  port: Number(POSTGRES_PORT),
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DB,
+  dialect: 'postgres',
+  dialectModule: pg,
+};
+
+export const configs = {
+  development: sequelizeOptions,
+  test: sequelizeOptions,
+  production: sequelizeOptions,
+};
