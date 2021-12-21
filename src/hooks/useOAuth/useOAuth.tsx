da83fc066e8d821a42b9c6e6c8b071a8c122c3d9
Verisana <account@0112.pro>
2021-12-21 21:15:51 +0500

    refactor: add comment to eslint disable line

--- src/hooks/useOAuth/useOAuth.tsx
+++ src/hooks/useOAuth/useOAuth.tsx
@@ -44,7 +44,9 @@ export const useOAuth = (redirectUri: string) => {
     }
 
     checkOAuthUser();
-    // eslint-disable-next-line
+    // Ругается на то, что надо передать dispatch, navigate и redirectUri в массив
+    // А мы хотим ее исполнить только один раз после рендеринга, а не при обновлении пропсов
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return startOAuth;
