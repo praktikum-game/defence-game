a802809824b845b658eda163f1286a20abc01b5f
Verisana <account@0112.pro>
2021-12-23 13:35:43 +0500

    feat: use api/v1 only in use

--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -1,14 +1,13 @@
 import express, { Router } from 'express';
 import { UserAPI } from '../controllers/UserAPI';
-import { apiEndpoint } from './consts';
 
 const jsonParser = express.json();
 
 export const userRoutes = (router: Router) => {
-  router.get(`${apiEndpoint}/user`, UserAPI.getAll);
-  router.get(`${apiEndpoint}/user/:id`, UserAPI.getById);
-  router.get(`${apiEndpoint}/user/:praktikumId`, UserAPI.getByPraktikumId);
-  router.post(`${apiEndpoint}/user`, jsonParser, UserAPI.create);
-  router.patch(`${apiEndpoint}/user/:praktikumId`, jsonParser, UserAPI.update);
-  router.delete(`${apiEndpoint}/user/:praktikumId`, UserAPI.delete);
+  router.get(`/user`, UserAPI.getAll);
+  router.get(`/user/:id`, UserAPI.getById);
+  router.get(`/user/:praktikumId`, UserAPI.getByPraktikumId);
+  router.post(`/user`, jsonParser, UserAPI.create);
+  router.patch(`/user/:praktikumId`, jsonParser, UserAPI.update);
+  router.delete(`/user/:praktikumId`, UserAPI.delete);
 };
