a008eabc5ffe18a0f93564fab8fc125aff9ab757
Verisana <account@0112.pro>
2021-12-23 13:06:19 +0500

    feat: create api endpoint

--- src/server/router/theme.ts
+++ src/server/router/theme.ts
@@ -1,12 +1,13 @@
 import express, { Router } from 'express';
 import { SiteThemeAPI } from 'server/controllers/SiteThemeAPI';
+import { apiEndpoint } from './consts';
 
 const jsonParser = express.json();
 export const themeRoutes = (router: Router) => {
-  router.get('/theme', SiteThemeAPI.getAll);
-  router.get('/theme/:id', SiteThemeAPI.getById);
-  router.get('/theme/:theme', SiteThemeAPI.getByThemeName);
-  router.post('/theme', jsonParser, SiteThemeAPI.create);
-  router.patch('/theme/:theme', jsonParser, SiteThemeAPI.update);
-  router.delete('/theme/:theme', SiteThemeAPI.delete);
+  router.get(`${apiEndpoint}/theme`, SiteThemeAPI.getAll);
+  router.get(`${apiEndpoint}/theme/:id`, SiteThemeAPI.getById);
+  router.get(`${apiEndpoint}/theme/:theme`, SiteThemeAPI.getByThemeName);
+  router.post(`${apiEndpoint}/theme`, jsonParser, SiteThemeAPI.create);
+  router.patch(`${apiEndpoint}/theme/:theme`, jsonParser, SiteThemeAPI.update);
+  router.delete(`${apiEndpoint}/theme/:theme`, SiteThemeAPI.delete);
 };
