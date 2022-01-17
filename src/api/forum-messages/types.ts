c82c7cd445c8b820c7d4a200053fda7501ff033a
Verisana <account@0112.pro>
2022-01-18 02:34:29 +0500

    feat: update forumMessageModel

--- src/api/forum-messages/types.ts
+++ src/api/forum-messages/types.ts
@@ -1,6 +1,18 @@
-export type MessageItem = {
-  user: string;
-  date: string;
-  id: string;
-  text: string;
+export type MessageModel = {
+  content: string;
+  createdAt: string;
+  id: number;
+  replyCommentId: number;
+  user: {
+    avatar: string | null;
+    name: string;
+    id: number;
+  };
+  forum_thread: {
+    subject: string;
+    content: string;
+    createdAt: string;
+    userId: number;
+    id: number;
+  };
 };
