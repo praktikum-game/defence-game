d156fc1b98a43534cc17d377c9b6a990a97b51b9
Verisana <account@0112.pro>
2022-01-17 19:40:06 +0500

    fix: set header after response

--- src/server/middlewares/only-auth-user-middleware.ts
+++ src/server/middlewares/only-auth-user-middleware.ts
@@ -1,11 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import { getUserDataSsr } from 'server/utilities/getUserData';
 import { HttpStatus } from '../http-statuses';
 
 export const onlyAuthUserMiddleware =
-  () => async (request: Request, response: Response, next: NextFunction) => {
+  () => async (_0: Request, response: Response, next: NextFunction) => {
     if (response.locals.user === undefined) {
-      return response.sendStatus(HttpStatus.Forbidden).send('Not authorize');
+      return response.sendStatus(HttpStatus.Forbidden);
     }
     next();
   };
