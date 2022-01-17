ae93489bd533f09fd7a4aad9ef993fc5142a0ee2
Verisana <account@0112.pro>
2022-01-17 19:11:57 +0500

    feat: create get-user-middleware

--- src/server/middlewares/get-user-middleware.ts
+++ src/server/middlewares/get-user-middleware.ts
@@ -2,15 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 import { getUserDataSsr } from 'server/utilities/getUserData';
 import { HttpStatus } from '../http-statuses';
 
-export const authMiddleware =
+export const getUserMiddleware =
   () => async (request: Request, response: Response, next: NextFunction) => {
+    response.locals.user = undefined;
     try {
       const { data, status } = await getUserDataSsr(request.headers.cookie);
       if (status === HttpStatus.OK) {
         response.locals.user = data;
-        next();
       }
-    } catch (e: unknown) {
-      return response.sendStatus(HttpStatus.Forbidden).send('Not authorize');
-    }
+    } catch (e: unknown) {}
+    next();
   };
