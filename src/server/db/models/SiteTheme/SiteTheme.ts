5a89efb291ce0243553bb7c243d0fb5187db7739
ShvetsovYura <shvetsov.yura@hotmail.com>
2022-01-19 20:40:38 +0300

    start migrate

--- src/server/db/models/SiteTheme/SiteTheme.ts
+++ src/server/db/models/SiteTheme/SiteTheme.ts
@@ -3,12 +3,81 @@ import { sequelize } from '../../sequelize';
 
 import { SiteThemeAttributes, SiteThemeCreationAttributes } from './types';
 
-export const SiteTheme: ModelDefined<SiteThemeAttributes, SiteThemeCreationAttributes> =
-  sequelize.define(
-    'SiteTheme',
-    {
-      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-      theme: { type: DataTypes.STRING, allowNull: false, unique: true },
-    },
-    { underscored: true, indexes: [{ fields: ['theme'] }] },
-  );
+// export const SiteTheme: ModelDefined<SiteThemeAttributes, SiteThemeCreationAttributes> =
+//   sequelize.define(
+//     'SiteTheme',
+//     {
+//       id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+//       theme: { type: DataTypes.STRING, allowNull: false, unique: true },
+//     },
+//     { underscored: true, indexes: [{ fields: ['theme'] }] },
+//   );
+
+
+  import {
+    Association,
+    DataTypes,
+    HasManyAddAssociationMixin,
+    HasManyCountAssociationsMixin,
+    HasManyCreateAssociationMixin,
+    HasManyGetAssociationsMixin,
+    HasManyHasAssociationMixin,
+    Model,
+    Optional,
+    Sequelize,
+  } from 'sequelize';
+  // import { User1 } from '../User1/User1';
+  
+
+  
+  export class SiteTheme
+    extends Model<SiteThemeAttributes, SiteThemeCreationAttributes>
+    implements SiteThemeAttributes
+  {
+    declare id: number;
+  
+    declare theme: string;
+  
+    declare readonly createdAt: Date;
+  
+    declare readonly updatedAt: Date;
+  
+    declare getUsers1: HasManyGetAssociationsMixin<User1>; // Note the null assertions!
+  
+    declare addUser: HasManyAddAssociationMixin<User1, number>;
+  
+    declare hasUser: HasManyHasAssociationMixin<User1, number>;
+  
+    declare countUsers1: HasManyCountAssociationsMixin;
+  
+    declare createUsers1: HasManyCreateAssociationMixin<User1>;
+  
+    declare readonly users1?: User[];
+  
+    declare static associations: {
+      users: Association<SiteTheme, User>;
+    };
+  }
+  
+  export function initSiteThemeModel(sequelize: Sequelize) {
+    SiteTheme.init(
+      {
+        id: {
+          type: DataTypes.INTEGER.UNSIGNED,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        theme: {
+          type: DataTypes.STRING,
+          unique:true,
+          allowNull: false,
+        },
+      },
+      {
+        tableName: 'site_theme',
+        sequelize,
+        underscored: true,
+      },
+    );
+  }
+  
--- src/server/db/models/User/User.ts
+++ src/server/db/models/User/User.ts
@@ -1,27 +1,67 @@
-import { ModelDefined, DataTypes } from 'sequelize';
+import { ModelDefined, DataTypes, Sequelize, Model } from 'sequelize';
 import { sequelize } from '../../sequelize';
 
 import { UserCreationAttributes } from './types';
 import { SiteTheme } from '../SiteTheme';
 import { UserAttributes } from 'shared/types/UserAttributes';
 
-const User: ModelDefined<UserAttributes, UserCreationAttributes> = sequelize.define(
-  'User',
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: false,
-      unique: true,
-    },
-    name: { type: DataTypes.STRING(255), allowNull: false },
-    avatar: { type: DataTypes.TEXT, allowNull: true },
-    SiteThemeId: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 },
-  },
-  { underscored: true },
-);
-SiteTheme.hasMany(User);
-User.belongsTo(SiteTheme, { foreignKey: { allowNull: false } });
+// const User: ModelDefined<UserAttributes, UserCreationAttributes> = sequelize.define(
+//   'User',
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: false,
+//       unique: true,
+//     },
+//     name: { type: DataTypes.STRING(255), allowNull: false },
+//     avatar: { type: DataTypes.TEXT, allowNull: true },
+//     SiteThemeId: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 },
+//   },
+//   { underscored: true },
+// );
+// SiteTheme.hasMany(User);
+// User.belongsTo(SiteTheme, { foreignKey: { allowNull: false } });
 
-export { User };
+// export { User };
+
+
+export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes{
+  declare id: number,
+  declare name:string;
+  declare avatar:string;
+
+  declare siteThemeId: number;
+
+  declare associations:{
+    
+  }
+}
+
+export function initUserModel(sequelize:Sequelize){
+  User.init({
+    id:{
+      type:DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:false,
+      unique:true
+    },
+    name:{
+      type:DataTypes.STRING,
+      allowNull:false,
+
+    },
+    avatar:{
+      type:DataTypes.STRING,
+      allowNull:true,
+      
+    },
+    siteThemeId:{
+      type:DataTypes.NUMBER,
+      allowNull:false
+    }
+  },{
+    sequelize, tableName:'users', underscored:true
+  })
+}
