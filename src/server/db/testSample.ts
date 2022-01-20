7019e83a414517b1a24c82a66e781bb6c57d2977
vladpotapov <vladpotapov@yandex-team.ru>
2022-01-20 18:06:48 +0300

    types example

--- src/server/db/testSample.ts
+++ src/server/db/testSample.ts
@@ -1,88 +1,117 @@
-import { commentService, forumThreadService, siteThemeService, userService } from './services';
+import { SiteTheme } from './models/SiteTheme';
+// import { User } from './models/User';
+import { siteThemeService } from './services';
 
 export async function addTestSamples() {
-  const existedThemes = await siteThemeService.readAll();
-  if (existedThemes.length !== 0) {
-    // eslint-disable-next-line no-console
-    console.log('Data exits. No need to add');
-    return;
-  }
+  // нормуль
+  const theme = await SiteTheme.findByPk(1);
+  const users = await theme?.getUsers();
 
-  await siteThemeService.bulkCreate([{ theme: 'light' }, { theme: 'dark' }]);
+  console.log(users, theme?.id);
 
-  const lightTheme = await siteThemeService.readById(1);
-  const darkTheme = await siteThemeService.readById(2);
+  // сервис возвращает уже другой тип (обобщенный) у которого нет нужных методов
+  // const theme1: Model<SiteThemeAttributes, SiteThemeCreationAttributes> | null = await siteThemeService.readById(1);
+  const theme1 = await siteThemeService.readById(1);
 
-  lightTheme?.createUser({ id: 123, name: 'pipa', siteThemeId: 1 });
-  lightTheme?.createUser({ id: 234, name: 'pipa1', siteThemeId: 1 });
+  theme1?.getUsers();
 
-  try {
-    const user = await userService.findByPraktikumId(123);
-    await user?.createForumThread({ subject: 'Title', content: 'description', userId: 1235 });
-  } catch (e: unknown) {
-    const error = e as Error;
-    console.log(error);
-  }
+  // const users1 = theme1?.getUser.... // нету
 
-  if (darkTheme && lightTheme) {
-    await userService.bulkCreate([
-      {
-        id: 1233,
-        name: 'TestUser1',
-        avatar: null,
-        siteThemeId: 1,
-      },
-      {
-        id: 2,
-        name: 'TestUser2',
-        siteThemeId: 2,
-      },
-      {
-        id: 3,
-        name: 'TestUser3',
-        siteThemeId: 1,
-      },
-    ]);
-  }
+  // theme1.getDataValue('')
 
-  const user = await userService.readOne();
-  if (user) {
-    await forumThreadService.create({
-      content: 'ForumContent1',
-      subject: 'ForumSubject1',
-      userId: user.id,
-    });
-  }
-  const forumThread = await forumThreadService.readOne();
+  // console.log(theme1?.i)
 
-  if (user && forumThread) {
-    await commentService.bulkCreate([
-      {
-        content: 'TestComment1',
-        replyCommentId: null,
-        userId: user.id,
-        forumThreadId: forumThread.id,
-      },
-      {
-        content: 'TestComment2',
-        replyCommentId: null,
-        userId: user.getDataValue('id'),
-        forumThreadId: forumThread.id,
-      },
-    ]);
-  }
 
-  const commentTest = await commentService.readOne();
-  if (commentTest && user && forumThread) {
-    await commentService.bulkCreate([
-      {
-        content: 'TestComment3',
-        replyCommentId: commentTest.getDataValue('id'),
-        userId: user.getDataValue('id'),
-        forumThreadId: forumThread.getDataValue('id'),
-      },
-    ]);
-  }
 
-  console.log(await user?.getSiteTheme());
+  // const existedThemes = await siteThemeService.readAll();
+  // if (existedThemes.length !== 0) {
+  //   // eslint-disable-next-line no-console
+  //   console.log('Data exits. No need to add');
+  //   return;
+  // }
+
+  // const theme = await SiteTheme.getUsers()
+
+  // await siteThemeService.bulkCreate([{ theme: 'light' }, { theme: 'dark' }]);
+
+  // const lightTheme = await siteThemeService.readById(1)
+  // const darkTheme = await siteThemeService.readById(2);
+
+  // try {
+  //   const user = await userService.readById(1)
+  //   user.add
+  //   await lightTheme?.createUser({ name: 'hoho', id: 1235, siteThemeId: 1, avatar: null });
+  //   SiteTheme.bul
+  // } catch (e: unknown) {
+  //   const error = e as Error;
+  //   console.log(error);
+  // }
+
+  // try {
+  //   const user = await User.findByPk(1235);
+  //   await user?.createForumThread({ subject: 'Title', content: 'description', userId: 1235 });
+  // } catch (e: unknown) {
+  //   const error = e as Error;
+  //   console.log(error);
+  // }
+
+  // if (darkTheme && lightTheme) {
+  //   await userService.bulkCreate([
+  //     {
+  //       id: 123,
+  //       name: 'TestUser1',
+  //       avatar: null,
+  //       SiteThemeId: 1,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'TestUser2',
+  //       SiteThemeId: 2,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'TestUser3',
+  //       SiteThemeId: 1,
+  //     },
+  //   ]);
+  // }
+
+  // const user = await userService.readOne();
+  // if (user) {
+  //   await forumThreadService.create({
+  //     content: 'ForumContent1',
+  //     subject: 'ForumSubject1',
+  //     UserId: user.getDataValue('id'),
+  //   });
+  // }
+  // const forumThread = await forumThreadService.readOne();
+
+  // if (user && forumThread) {
+  //   await commentService.bulkCreate([
+  //     {
+  //       content: 'TestComment1',
+  //       replyCommentId: null,
+  //       userId: user.getDataValue('id'),
+  //       forumThreadId: forumThread.getDataValue('id'),
+  //     },
+  //     {
+  //       content: 'TestComment2',
+  //       replyCommentId: null,
+  //       userId: user.getDataValue('id'),
+  //       forumThreadId: forumThread.getDataValue('id'),
+  //     },
+  //   ]);
+  // }
+
+  // const commentTest = await commentService.readOne();
+  // if (commentTest && user && forumThread) {
+  //   await commentService.bulkCreate([
+  //     {
+  //       content: 'TestComment3',
+  //       replyCommentId: commentTest.getDataValue('id'),
+  //       userId: user.getDataValue('id'),
+  //       forumThreadId: forumThread.getDataValue('id'),
+  //     },
+  //   ]);
+  // }
 }
